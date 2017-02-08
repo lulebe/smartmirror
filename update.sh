eb2e8c0fbc041e07b0ef83ec040abb4e3ed8b20a
Leander Berg <reg@lulebe.net>
2017-02-08 15:54:50 +0100

    improved update script

--- update.sh
+++ update.sh
@@ -1,3 +1,10 @@
 git fetch --all
 git reset --hard origin/master
+export npm_config_target=1.4.15
+export npm_config_disturl=https://atom.io/download/electron
+export npm_config_runtime=electron
+export npm_config_build_from_source=true
+HOME=.electron-gyp npm install
+export npm_config_build_from_source=false
+rm -rf .electron-gyp
 reboot
