8f62adc9a26473c599dd19ff6c1f53e9f30f43a9
Leander Berg <reg@lulebe.net>
2017-02-22 10:52:08 +0100

    fixed pin numbering

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-gpio mode 4 up
-gpio export 4 in
-gpio mode 17 up
-gpio export 17 in
-gpio mode 27 up
-gpio export 27 in
-gpio mode 22 up
-gpio export 22 in
-gpio mode 23 up
-gpio export 23 in
+gpio -g mode 4 up
+gpio -g export 4 in
+gpio -g mode 17 up
+gpio -g export 17 in
+gpio -g mode 27 up
+gpio -g export 27 in
+gpio -g mode 22 up
+gpio -g export 22 in
+gpio -g mode 23 up
+gpio -g export 23 in
 cd /home/pi/smartmirror
 npm start
